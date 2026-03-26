f1d51ec09885d9adb4f7092d534cd39fe28690c3
mugdad <128133561+mugdad1@users.noreply.github.com>
2026-03-26 22:58:46 +0000

    Update 02-configs.sh

--- 02-configs.sh
+++ 02-configs.sh
@@ -1,36 +1,33 @@
-Red='\e[0;31m';
-Gre='\e[0;32m';
-Whi='\e[0;37m';
+#!/bin/bash
+
+# Colors
+Red='\e[0;31m'
+Gre='\e[0;32m'
+Whi='\e[0;37m'
+End='\e[0m'
 
 echo -e "[${Gre}*${Whi}] Setting up config files"
 
-echo -e "➞ [${Red}*${Whi}] Removing existing config directories"
+# Ensure we are in the dotfiles directory
+DOTFILES="$HOME/.dotfiles"
+cd "$DOTFILES" || { echo "Dotfiles dir not found"; exit 1; }
 
-cd ~/.dotfiles
+# 1. Handle .config directory
+echo -e "➞ [${Red}*${Whi}] Cleaning and Symlinking .config"
+if [ -d ".config" ]; then
+    # Create ~/.config if it doesn't exist
+    mkdir -p ~/.config
+    
+    # Run stow from the dotfiles root. 
+    # This will link everything inside .dotfiles/.config/* into ~/.config/
+    stow -v -R -t ~/.config .config
+fi
 
-# Remove existing .config directories
-for dir in $(find .config -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
-do
-	rm -rf ~/.config/$dir
-done
+# 2. Handle .home directory
+echo -e "➞ [${Gre}*${Whi}] Cleaning and Symlinking .home"
+if [ -d ".home" ]; then
+    # This links the contents of .home (like .bashrc, .zshrc) directly into ~
+    stow -v -R -t ~ .home
+fi
 
-# Remove existing .home directories
-for dir in $(find .home -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
-do
-	rm -rf ~/$dir
-done
-
-echo -e "➞ [${Gre}*${Whi}] Symlinking directories in .config"
-
-cd ~/.dotfiles/.config
-for dir in $(find . -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
-do
-	stow -t ~/.config/$dir $dir
-done
-
-echo -e "➞ [${Gre}*${Whi}] Symlinking directories in .home"
-
-cd ~/.dotfiles
-stow -t ~ .home
-
-echo -e "[${Gre}*${Whi}] Finished setting up configs"
+echo -e "[${Gre}*${Whi}] Finished setting up configs${End}"
