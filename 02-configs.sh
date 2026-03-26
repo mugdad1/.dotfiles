e3b6403c047a9fae759207febeb8e46e119c2150
mugdad <128133561+mugdad1@users.noreply.github.com>
2026-03-26 21:55:56 +0000

    Update 02-configs.sh

--- 02-configs.sh
+++ 02-configs.sh
@@ -4,56 +4,33 @@ Whi='\e[0;37m';
 
 echo -e "[${Gre}*${Whi}] Setting up config files"
 
-echo -e "➞ [${Red}*${Whi}] Removing ~/dotfiles-backup for backing up current configs"
-
-rm -rf ~/dotfiles-backup
-
-echo -e "➞ [${Gre}*${Whi}] Backing up existing directories"
-
-mkdir -p ~/dotfiles-backup
-mkdir -p ~/dotfiles-backup/config
-mkdir -p ~/dotfiles-backup/home
-
-# --------
+echo -e "➞ [${Red}*${Whi}] Removing existing config directories"
 
 cd ~/.dotfiles
 
-for dir in $(find .config -maxdepth 1 -mindepth 1 | awk -F "/" '{print $NF}')
+# Remove existing .config directories
+for dir in $(find .config -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
 do
-	mv ~/.config/$dir ~/dotfiles-backup/config
+	rm -rf ~/.config/$dir
 done
 
-for dir in $(find .config -maxdepth 1 -mindepth 1 | awk -F "/" '{print $NF}')
+# Remove existing .home directories
+for dir in $(find .home -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
 do
-	mkdir -p ~/.config/$dir
+	rm -rf ~/$dir
 done
-
-# --------
-
-for dir in $(find .home -mindepth 1 -maxdepth 1 | awk -F "/" '{print $NF}')
-do
-	mv ~/$dir ~/dotfiles-backup/home
-done
-
-echo -e "➞ [${Gre}*${Whi}] Backups saved in ~/dotfiles-backup"
 
 echo -e "➞ [${Gre}*${Whi}] Symlinking directories in .config"
 
-# --------
-
 cd ~/.dotfiles/.config
-
-for dir in $(find . -maxdepth 1 -mindepth 1 | awk -F "/" '{print $NF}')
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d | sed 's|.*/||')
 do
 	stow -t ~/.config/$dir $dir
 done
 
-# --------
-
-cd ~/.dotfiles
-
 echo -e "➞ [${Gre}*${Whi}] Symlinking directories in .home"
 
+cd ~/.dotfiles
 stow -t ~ .home
 
 echo -e "[${Gre}*${Whi}] Finished setting up configs"
