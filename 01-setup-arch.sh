d591a520b94e9569d2788e9cb2c842e9001a6d4a
mugdad <128133561+mugdad1@users.noreply.github.com>
2026-03-26 21:53:53 +0000

    Update 01-setup-arch.sh

--- 01-setup-arch.sh
+++ 01-setup-arch.sh
@@ -5,26 +5,20 @@ Whi='\e[0;37m';
 
 # Synchronize package databases
 echo -e "[${Red}*${Whi}] Updating system.."
-sudo pacman -Syu
-
- #Install aura AUR helper
-echo -e "[${Red}+${Whi}] Installing aura"
-git clone https://aur.archlinux.org/aura-bin.git /tmp/aura-git-cloned
-cd /tmp/aura-git-cloned/
-makepkg -sfci --noconfirm --needed
+paru -Syu
 
 # Install pkgs
-echo -e "[${Red}+${Whi}] Installing repo packages"
-for repo_pkg in $(cat ~/.dotfiles/.assets/pkg_lists/pkg_list)
+echo -e "[${Red}+${Whi}] Installing packages"
+for pkg in $(cat ~/.dotfiles/.assets/pkg_lists/pkg_list)
 do
-	sudo pacman -S --noconfirm --needed $repo_pkg
+	paru -S --noconfirm --needed $pkg
 done
 
 # Install pkgs from the AUR
- echo -e "[${Cya}+${Whi}] Installing AUR packages"
- for aur_pkg in $(cat ~/.dotfiles/.assets/pkg_lists/aur_pkg_list)
+echo -e "[${Cya}+${Whi}] Installing AUR packages"
+for aur_pkg in $(cat ~/.dotfiles/.assets/pkg_lists/aur_pkg_list)
 do
-	sudo aura -Acax --noconfirm $aur_pkg
+	paru -S --noconfirm $aur_pkg
 done
 
 # Setup ZSH
@@ -39,7 +33,6 @@ sudo chsh -s /usr/bin/zsh
 
 # Clone GTK theme(s) and icons
 echo -e "[${Gre}*${Whi}] Cloning GTK theme and icons"
-
 sudo git clone https://codeberg.org/tplasdio/numigsur-icon-theme.git /usr/share/icons/numigsur-icon-theme
 sudo git clone https://github.com/EliverLara/Nordic /usr/share/themes/Nordic
 
