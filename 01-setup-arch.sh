4aa6b809084a09274fc0cc3e5c19a4896f56f74a
mugdad <128133561+mugdad1@users.noreply.github.com>
2026-03-27 03:27:29 +0300

    Update 01-setup-arch.sh

--- 01-setup-arch.sh
+++ 01-setup-arch.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 Red='\e[0;31m'; Gre='\e[0;32m'; Cya='\e[0;36m'; Whi='\e[0;37m'; End='\e[0m'
 
-echo -e "[${Cya}*${Whi}] Installing River 0.4.1 + Beansprout Stack..."
-
-# Your original package list installation (KEEPING THIS)
+echo -e "[${Cya}*${Whi}] Installing River 0.4.1 + Beansprout..."
 PKG_LIST="$HOME/.dotfiles/.assets/pkg_lists/pkg_list"
+
+# Install your existing list
 grep -v '^#' "$PKG_LIST" | xargs paru -S --noconfirm --needed
 
-# Install Zig (Required to build Beansprout if not in your pkg_list)
-sudo pacman -S --needed zig wayland-protocols
-
-# --- New: Build Beansprout from Source ---
+# Build Beansprout from Source
 echo -e "[${Gre}*${Whi}] Building Beansprout WM..."
+sudo pacman -S --needed zig wayland-protocols wlroots libxkbcommon pixman
+
 if [ ! -d "/tmp/beansprout" ]; then
-    git clone https://codeberg.org/river/beansprout /tmp/beansprout
+    git clone https://codeberg.org/beansprout/beansprout /tmp/beansprout
 fi
 
 cd /tmp/beansprout
 zig build -Doptimize=ReleaseSafe
 sudo cp zig-out/bin/beansprout /usr/local/bin/
-
-# Return to your script directory
 cd - > /dev/null
 
-# Proceed to your configs
 chmod +x ./02-configs.sh
 ./02-configs.sh
